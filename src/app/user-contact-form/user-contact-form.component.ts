325c9bc7273377b38fc44afd0fa1e5de425604e6
Ryan Smee <theryansmee@gmail.com>
2020-09-27 15:48:33 +0100

    Created and styled all the basic form elements ready for reactive form code

--- src/app/user-contact-form/user-contact-form.component.ts
+++ src/app/user-contact-form/user-contact-form.component.ts
@@ -2,16 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 
 @Component({
-    selector: 'app-user-contact-form',
-    templateUrl: './user-contact-form.component.html',
-    styleUrls: ['./user-contact-form.component.scss']
+	selector: 'app-user-contact-form',
+	templateUrl: './user-contact-form.component.html',
+	styleUrls: ['./user-contact-form.component.scss']
 })
 export class UserContactFormComponent implements OnInit {
 
-    constructor () { }
+	constructor () { }
 
-    ngOnInit(): void {
-
-    }
+	ngOnInit (): void { }
 
 }
